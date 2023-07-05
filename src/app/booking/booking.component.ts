22d35ca6c38fc35ae8dfef86842131dc31a89ded
Jayanti_Kore <jayanti.khutwad@riaadvisory.com>
2023-07-05 12:02:04 +0530

    validations

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BookingServService } from '../booking-serv.service';
 
 @Component({
@@ -9,29 +9,31 @@ import { BookingServService } from '../booking-serv.service';
 })
 export class BookingComponent implements OnInit {
   profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    phonwNo: new FormControl(''),
-    email: new FormControl(''),
-    date:new FormControl(''),
-    area: new FormControl(''),
-    city: new FormControl(''),
-    pin: new FormControl(''),
-    state: new FormControl(''),
+    firstName: new FormControl('', [Validators.required]),
+    phonwNo: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    date: new FormControl('', [Validators.required, Validators.email]),
+    area: new FormControl('', [Validators.required]),
+    city: new FormControl('', [Validators.required]),
+    pin: new FormControl('', [Validators.required]),
+    state: new FormControl('', [Validators.required]),
   });
+
+
   constructor(public bkServ: BookingServService) { }
 
   ngOnInit(): void {
     if (this.bkServ.selectedBookIndex != null) {
       const currentBookings = this.bkServ.allBookings[this.bkServ.selectedBookIndex];
       this.profileForm = new FormGroup({
-        firstName: new FormControl(currentBookings.firstName),
-        phonwNo: new FormControl(currentBookings.phonwNo),
-        email: new FormControl(currentBookings.email),
-        date:new FormControl(currentBookings.date),
-        area: new FormControl(currentBookings.area),
-        city: new FormControl(currentBookings.city),
-        pin: new FormControl(currentBookings.pin),
-        state: new FormControl(currentBookings.state),
+        firstName: new FormControl(currentBookings.firstName, [Validators.required]),
+        phonwNo: new FormControl(currentBookings.phonwNo, [Validators.required]),
+        email: new FormControl(currentBookings.email, [Validators.required, Validators.email]),
+        date: new FormControl(currentBookings.date, [Validators.required]),
+        area: new FormControl(currentBookings.area, [Validators.required]),
+        city: new FormControl(currentBookings.city, [Validators.required]),
+        pin: new FormControl(currentBookings.pin, [Validators.required]),
+        state: new FormControl(currentBookings.state, [Validators.required]),
       });
     }
   }
@@ -45,14 +47,14 @@ export class BookingComponent implements OnInit {
       this.bkServ.selectedBookIndex = null;
     }
     this.profileForm = new FormGroup({
-      firstName: new FormControl(''),
-      phonwNo: new FormControl(''),
-      email: new FormControl(''),
-      date: new FormControl(''),
-      area: new FormControl(''),
-      city: new FormControl(''),
-      pin: new FormControl(''),
-      state: new FormControl(''),
+      firstName: new FormControl('', [Validators.required]),
+      phonwNo: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      date: new FormControl('', [Validators.required]),
+      area: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      pin: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
     });
 
   }
